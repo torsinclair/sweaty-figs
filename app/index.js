1283112ed7f0835a51b9f534ee3753b409a288a8
Tor Sinclair <tor@romanesco.se>
2016-05-06 14:09:10 -0700

    Install webpack

---
+++ app/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Main = React.createClass({
+  render () {
+    return (
+      <p>Hello World</p>
+    )
+  }
+})
+
+ReactDOM.render(
+  <Main />,
+  document.getElementById('app'))
